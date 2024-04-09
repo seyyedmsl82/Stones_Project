63e3247f878441b9e3b7562e7fc530b05239ee02
SeyyedReza <seyyed.msl82@gmail.com>
2024-04-09 13:53:33 +0330

    preparing Stone dataset

--- model.py
+++ model.py
@@ -1,20 +1,32 @@
 import torch
 from torchvision import datasets
+from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import os
 
-training_data = datasets.FashionMNIST(
-    root='./data',
-    train=True,
-    download=True,
-    transform=ToTensor()
-)
-test_data = datasets.FashionMNIST(
-    root='./data',
-    train=False,
-    download=True,
-    transform=ToTensor()
-)
 
-print(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+class StoneDataset(Dataset):
+    def __init__(self, data_address, gp="train"):
+        self.data = pd.read_csv(data_address)
+        self.gp = gp
+
+        self.img_path = self.data.iloc[:, 0]
+        self.label = self.data.iloc[:, -1]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        image_path = os.path.join(f"data/{self.gp}/", self.img_path[index])
+        label = self.label[index]
+        return image_path, label
+
+
+dataset = StoneDataset(r"data/test.csv", gp='test')
+# print(dataset[0])
+img = plt.imread(dataset[0][0])
+plt.imshow(img)
+plt.show()
