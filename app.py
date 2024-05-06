6de203927e1e987e5112c13408d00fba10ef8870
SeyyedReza <seyyed.msl82@gmail.com>
2024-05-06 21:53:47 +0330

    apply grad-cam on app

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask, request, jsonify, render_template, send_from_directory
 from PIL import Image
 import torch
 from torchvision import transforms
+import matplotlib.pyplot as plt
+from pytorch_grad_cam import GradCAM
+# locals
 from model import neural_net
 from utils import image_cropper
 
@@ -12,10 +15,18 @@ app = Flask(__name__)
 # Load the model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = neural_net.Net()
+# Unfreeze the desired layer
+model.unfreeze_layer('layer4.1.conv2.weight')
+model.unfreeze_layer('layer4.1.conv2.bias')
+
 checkpoint = torch.load("stone_model.pth", map_location=device)
 model.load_state_dict(checkpoint)
 model.to(device)
 model.eval()
+
+# Set the target layer for grad-cam
+target_layers = [model.base_model.layer4[-1]]
+cam = GradCAM(model=model, target_layers=target_layers)
 
 # Define class labels
 classes = {
@@ -41,7 +52,9 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 def predict(image_path):
     img_ = Image.open(image_path)
-    img = transform(img_).unsqueeze(0).to(device)
+    img = transform(img_).to(device).unsqueeze(0)
+    # Grad-CAM of the input image
+    gradcam(img)
 
     with torch.no_grad():
         logits = model(img)
@@ -50,6 +63,25 @@ def predict(image_path):
     predicted_class = torch.argmax(probability, dim=1).item()
 
     return classes[predicted_class], probability.cpu().numpy()[0][torch.argmax(probability, dim=1).item()]
+
+
+def gradcam(image):
+    grayscale_cam = cam(input_tensor=image)
+
+
+    # In this example grayscale_cam has only one image in the batch:
+    grayscale_cam = grayscale_cam[0, :]
+
+    # Overlay the heatmap on the original image
+    plt.imshow(image.squeeze().cpu().numpy().transpose(1, 2, 0))
+
+    # Overlay the heatmap with transparency
+    plt.imshow(grayscale_cam, cmap='jet', alpha=0.5)
+
+    # Add a colorbar to show the intensity scale of the heatmap
+    plt.colorbar()
+
+    plt.show()
 
 
 # Route to upload image and predict
@@ -71,17 +103,26 @@ def upload_image():
         if file:
             filename = file.filename
             processes_filename = f"{filename.split('.')[0]}_processed.jpg"
+            
+            # Save the input image in a temporary path
             filepath = os.path.join(UPLOAD_FOLDER + "/images/", filename)
-            print(filepath)
             file.save(filepath)
+
+            # Process the image
             selected_image = cv2.imread(filepath)
             processed_image = image_cropper(selected_image)
+            new_file_path = os.path.join(UPLOAD_FOLDER + "/images/", processes_filename)
+            cv2.imwrite(new_file_path, processed_image) # Save the procesed image
+
+            # Predict the class of input image
+            predicted_class, accuracy = predict(new_file_path)
+
+            # Resize to have a better demonstration
             h, w = processed_image.shape[:2]
             processed_image = cv2.resize(processed_image, (w//2, h//2))
-            new_file_path = os.path.join(UPLOAD_FOLDER + "/images/", processes_filename)
-            # print(processed_image)
-            cv2.imwrite(new_file_path, processed_image)
-            predicted_class, accuracy = predict(new_file_path)
+            cv2.imwrite(new_file_path, processed_image) # Save the procesed image
+
+            # Render template
             return render_template("result.html", image=f"images/{processes_filename}", prediction=predicted_class, accuracy=accuracy*100)
 
     return render_template("index.html")
