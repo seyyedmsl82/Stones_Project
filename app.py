023c8bcc78dff64f42472feadf46724344b846f1
SeyyedReza <seyyed.msl82@gmail.com>
2024-06-05 23:36:56 +0330

    double images, fix percentage, fix width and height problems, fix grad-cam of the input image

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import os, glob
+import os
+import glob
 import cv2
-import pickle
 import io
 from flask import Flask, request, jsonify, render_template, send_from_directory
 from PIL import Image
@@ -8,46 +8,14 @@ import torch
 from torchvision import transforms
 import matplotlib.pyplot as plt
 from pytorch_grad_cam import GradCAM
-import googleapiclient
 from googleapiclient.discovery import build
-import google
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from googleapiclient.http import MediaIoBaseUpload
-from google.oauth2 import service_account
 from google.oauth2.credentials import Credentials
 from googleapiclient.http import MediaFileUpload
 
 # locals
 from model import neural_net
-from utils import image_cropper
-
-# # Google Drive API setup
-# CLIENT_SECRET_FILE = "client_secret.json"
-# API_NAME = 'drive'
-# API_VERSION = 'v3'
-# SCOPES = ['https://www.googleapis.com/auth/drive']
-# drive = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-#
-# creds = None
-# if os.path.exists('token.pickle'):
-#     with open('token.pickle', 'rb') as token:
-#         creds = pickle.load(token)
-#
-# # If there are no (valid) credentials available, let the user log in.
-# if not creds or not creds.valid:
-#     if creds and creds.expired and creds.refresh_token:
-#         creds.refresh(Request())
-#     else:
-#         flow = InstalledAppFlow.from_client_secrets_file(
-#             CLIENT_SECRET_FILE, SCOPES)
-#         creds = flow.run_local_server()
-#
-#     # Save the credentials for the next run
-#     with open('token.pickle', 'wb') as token:
-#         pickle.dump(creds, token)
-#
-# service = build('drive', 'v3', credentials=creds)
+from utils import image_cropper, grad_cam
 
 CLIENT_ID = '142857969907-2f0geqqrfn8ius4lk9bv2jvlmbgrrki6.apps.googleusercontent.com'
 CLIENT_SECRET = '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'
@@ -63,24 +31,16 @@ creds = Credentials(None,
 # Initialize the Drive API client
 drive_service = build('drive', 'v3', credentials=creds)
 
-
 app = Flask(__name__)
 
 # Load the model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = neural_net.Net()
-# Unfreeze the desired layer
-model.unfreeze_layer('layer4.1.conv2.weight')
-model.unfreeze_layer('layer4.1.conv2.bias')
 
 checkpoint = torch.load("stone_model.pth", map_location=device)
 model.load_state_dict(checkpoint)
 model.to(device)
 model.eval()
-
-# Set the target layer for grad-cam
-target_layers = [model.base_model.layer4[-1]]
-# cam = GradCAM(model=model, target_layers=target_layers)
 
 # Define class labels
 classes = {
@@ -104,15 +64,6 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 
-def save_to_local(file, class_type):
-    if not os.path.exists(app.config['UPLOAD_FOLDER_INPUTS']):
-        os.makedirs(app.config['UPLOAD_FOLDER_INPUTS'])
-
-    file_path = os.path.join(app.config['UPLOAD_FOLDER_INPUTS'], f"{class_type}_{file.filename}")
-    file.save(file_path)
-    return file_path
-
-
 def upload_file(file_path):
     try:
         file_metadata = {'name': 'image.jpg'}
@@ -127,12 +78,11 @@ def upload_file(file_path):
         print(f'An error occurred: {error}')
 
 
-
 def predict(image_path, w, h):
     img_ = Image.open(image_path)
     img = transform(img_).to(device).unsqueeze(0)
-    # # Grad-CAM of the input image
-    # grad_cam(img, w, h)
+    # Grad-CAM of the input image
+    grad_cam(model, image_path=image_path, transform=transform, device=device)
 
     with torch.no_grad():
         logits = model(img)
@@ -140,38 +90,10 @@ def predict(image_path, w, h):
     probability = torch.softmax(logits, dim=1)
     predicted_class = torch.argmax(probability, dim=1).item()
     accuracy = probability.cpu().numpy()[0][torch.argmax(probability, dim=1).item()]
-    accuracy = float("{:.4f}".format(accuracy))
-    print(float(accuracy))
+    accuracy *= 100
+    accuracy = float("{:.2f}".format(accuracy))
 
     return classes[predicted_class], accuracy
-
-
-# def grad_cam(image, w, h):
-#     grayscale_cam = cam(input_tensor=image)
-#
-#     # In this example grayscale_cam has only one image in the batch:
-#     grayscale_cam = grayscale_cam[0, :]
-#
-#     # Overlay the heatmap on the original image
-#     plt.imshow(image.squeeze().cpu().numpy().transpose(1, 2, 0))
-#
-#     # Remove axis numbers
-#     plt.xticks([])
-#     plt.yticks([])
-#
-#     # Overlay the heatmap with transparency
-#     plt.imshow(grayscale_cam, cmap='jet', alpha=0.5)
-#
-#     # # Add a colorbar to show the intensity scale of the heatmap
-#     # plt.colorbar()
-#
-#     plt.savefig("heatmap.png")
-#     plt.show()
-#
-#     img = cv2.imread("heatmap.png")
-#     img = cv2.resize(img, (int(w * 3 / 2), int(h * 3 / 2)))
-#     cv2.imshow("heatmap", img)
-#     cv2.waitKey(0)
 
 
 # Route to upload image and predict
@@ -209,12 +131,18 @@ def upload_image():
             predicted_class, accuracy = predict(new_file_path, w, h)
 
             # Resize to have a better demonstration
-            processed_image = cv2.resize(processed_image, (w // 2, h // 2))
+            h_, w_ = selected_image.shape[:2]
+            w_ = w_ // (h_ // 600)
+            h_ =  600
+            selected_image = cv2.resize(selected_image, (w_, h_))
+            processed_image = cv2.resize(processed_image, ((w_ * h) // h_, h_))
             cv2.imwrite(new_file_path, processed_image)  # Save the processed image
+            cv2.imwrite(filepath, selected_image)
 
             # Render template
-            return render_template("result.html", image=f"images/{processes_filename}", prediction=predicted_class,
-                                   accuracy=accuracy * 100)
+            return render_template("result.html", image1=f"images/{processes_filename}", image2=f"images/{filename}",
+                                   prediction=predicted_class,
+                                   accuracy=accuracy)
 
     return render_template("index.html")
 
@@ -260,8 +188,6 @@ def upload_file():
                 fields='id'
             ).execute()
             print('File ID:', response.get('id'))
-
-
 
             return render_template("greeting.html")
 
